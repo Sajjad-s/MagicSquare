28a81b8b3ba5c3f807c4f6d04c636cf7f45b4ff2
Sajjad-s <darma.sajjad@gmail.com>
2021-04-29 15:29:21 +0430

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] inputArray
+        System.out.println("Enter the 3x3 Matrix");
+        for (int i = 0; i <3 ; i++) {
+            for (int j = 0; j <3 ; j++) {
+                System.out.println("");
+            }
+
+        }
+    }
+}
+
+public static int checkSquare(int[][] inputArray){
+    int[][] baseArrays = {
+            {8,1,6,3,5,7,4,9,2},
+            {6,1,8,7,5,3,2,9,4},
+            {4,9,2,3,5,7,8,1,6},
+            {2,9,4,7,5,3,6,1,8},
+            {8,3,4,1,5,9,6,7,2},
+            {4,3,8,9,5,1,2,7,6},
+            {6,7,2,1,5,9,8,3,4},
+            {2,7,6,9,5,1,4,3,8}
+    };
+
+    return 0;
+}
