f7cb28af4de9157a96b965a6072709bfa9f03b38
Sajjad-s <darma.sajjad@gmail.com>
2021-05-05 23:10:51 +0430

    Change algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import org.junit.Assert;
-import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
-import java.util.Scanner;
-
 
 import java.util.Scanner;
 
@@ -11,99 +7,77 @@ public class Main {
 
         Main main = new Main();
         System.out.println("Enter the 3x3 Matrix");
-        int[] array = main.getInputArray();
+        int[][] array = main.getInputArray();
         showArray(array);
-        System.out.println("The answer is: " + main.checkSquare(array));
+        int  result = main.findMinimum(array);
+        System.out.println("The answer is: " + result);
 
     }
 
-    private static void showArray(int[] array) {
+    private static void showArray(int[][] array) {
         int counter = 0;
         System.out.println("Your input matrix is: ");
         try {
             String str = "|\t";
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    str += array[counter] + "\t";
+                    str += array[i][j] + "\t";
                     counter++;
                 }
                 System.out.println(str + "|");
                 str = "|\t";
             }
-
         } catch (Exception e) {
             System.out.println("Matrix is empty!!");
         }
     }
 
-    public int[] getInputArray() {
-        int[] inputArray = new int[9];
+    public int[][] getInputArray() {
+        int[][] inputArray = new int[3][3];
         Scanner scanner = new Scanner(System.in);
-        int counter = 0;
         for (int i = 0; i < 3; i++) {
             System.out.println("\n");
             for (int j = 0; j < 3; j++) {
                 System.out.print("Enter the [" + i + "] [" + j + "]: ");
-                inputArray[counter] = scanner.nextInt();
-                counter++;
+                inputArray[i][j] = scanner.nextInt();
             }
         }
         return inputArray;
     }
 
-    public int checkSquare(int[] inputArray) {
-        int[][] baseArrays = {
-                        {8, 1, 6,
-                         3, 5, 7,
-                         4, 9, 2},
-
-                        {6, 1, 8,
-                         7, 5, 3,
-                         2, 9, 4},
-
-                        {4, 9, 2,
-                         3, 5, 7,
-                         8, 1, 6},
-
-                        {2, 9, 4,
-                         7, 5, 3,
-                         6, 1, 8},
-
-                        {8, 3, 4,
-                         1, 5, 9,
-                         6, 7, 2},
-
-                        {4, 3, 8,
-                         9, 5, 1,
-                         2, 7, 6},
-
-                        {6, 7, 2,
-                         1, 5, 9,
-                         8, 3, 4},
-
-                        {2, 7, 6,
-                         9, 5, 1,
-                         4, 3, 8}
-
-
-        };
-        int best = 10000;
-        for (int i = 0; i < 8; i++) {
-            int difference = 0;
-            for (int j = 0; j < 9; j++) {
-                difference += Math.abs(inputArray[j] - baseArrays[i][j]);
-                if (difference < best) best = difference;
+    public static int findMinimumFromMagicSquare(int[][] array,
+                                                 int[][] magicSquare) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (array[i][j] != magicSquare[i][j])
+                    count++;
             }
-
         }
-        return best;
+        return count;
     }
 
-    @Test
-    public void positiveTest() {
-        Main main = new Main();
-        int[] array = {1, 7, 6, 9, 5, 1, 4, 3, 8};
-        int result = main.checkSquare(array);
-        Assert.assertEquals(1,result);
+    public static int findMinimum(int[][] array) {
+        int[][][] magicSquare = {
+                {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+                {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+                {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+                {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+                {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+                {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+                {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+                {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}},
+        };
+
+        // If all the elements need to be changed,
+        // there would be 9 changes, so we take the
+        // max as 9
+        int min = 9;
+        for (int i = 0; i < 8; i++) {
+            int x = findMinimumFromMagicSquare(array, magicSquare[i]);
+            if (x < min)
+                min = x;
+        }
+        return min;
     }
 }
